e9c6ee44697b90d3934da6d7e8d2935772a06c33
s1ckn3zz <alexander.kiri@hotmail.de>
2017-10-13 16:32:10 +0200

    add first field creating function

--- js/codebase.js
+++ js/codebase.js
@@ -1,3 +1,18 @@
 /**
  * Created by s1ckn3zz on 13.10.17.
  */
+document.addEventListener("DOMContentLoaded", function() {
+});
+
+
+var generateField = function(x,y){
+    var mainField = document.getElementById('gameField');
+    for(var i = 0; i<x; i++){
+        var iDiv = document.createElement('div');
+        iDiv.id = 'block';
+        iDiv.className = 'block';
+        for(var j= 0; j<y; j++){
+
+        }
+    }
+};
