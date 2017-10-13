9cf310bb4976c2461259a7991d6d0dee55653297
s1ckn3zz <alexander.kiri@hotmail.de>
2017-10-13 16:43:00 +0200

    first implementation of game field

--- js/codebase.js
+++ js/codebase.js
@@ -2,17 +2,22 @@
  * Created by s1ckn3zz on 13.10.17.
  */
 document.addEventListener("DOMContentLoaded", function() {
+    generateField(2,2);
 });
 
 
 var generateField = function(x,y){
     var mainField = document.getElementById('gameField');
     for(var i = 0; i<x; i++){
-        var iDiv = document.createElement('div');
-        iDiv.id = 'block';
-        iDiv.className = 'block';
+        var rowField = document.createElement('div');
+        rowField.id = 'row'+i;
+        rowField.className = 'row';
         for(var j= 0; j<y; j++){
-
+            var singleGameField = document.createElement('div');
+            singleGameField.id = 'row'+i+'field'+j;
+            singleGameField.className = 'gameField whiteField';
+            rowField.appendChild(singleGameField);
         }
+        mainField.appendChild(rowField);
     }
 };
