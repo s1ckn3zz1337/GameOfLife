b36e5b835541933bb3acd6b48e83282f5eadf780
JesusJones <jonas.jaekel@gmail.com>
2017-10-14 10:32:43 +0200

    changed counters generateField for better understanding

--- js/codebase.js
+++ js/codebase.js
@@ -11,13 +11,13 @@ var generateField = function(x,y){
     var mainField = document.getElementById('gameField');
     deleteGameField();
 
-    for(var i = 0; i<x; i++){
+    for(var row = 0; row<x; row++){
         var rowField = document.createElement('div');
-        rowField.id = 'row'+i;
+        rowField.id = 'row'+row;
         rowField.className = 'row';
-        for(var j= 0; j<y; j++){
+        for(var column= 0; column<y; column++){
             var singleGameField = document.createElement('div');
-            singleGameField.id = 'row'+i+'field'+j;
+            singleGameField.id = 'row'+row+'field'+column;
             singleGameField.className = 'gameField whiteField';
             rowField.appendChild(singleGameField);
         }
